2666f5955eeed5ff8ecc24c51fb34c4af33a6532
Maksim Karimov <67639452+maxkarim@users.noreply.github.com>
2022-10-03 04:38:09 -0700

    Add files via upload

---
+++ AD_CleanDNS.ps1
@@ -0,0 +1,29 @@
+﻿$DeadDNSRecords = @("khsz-dc.kherson-shipyard.com.", "khsz-dc5.kherson-shipyard.com." , "khsz-dc2.kherson-shipyard.com." ,"rdp.kherson-shipyard.com.")
+$DeadIpAdresses = @("172.22.33.102","172.22.33.103")
+$DNSZones = @(“_msdcs.kherson-shipyard.com”,“kherson-shipyard.com”)
+
+
+foreach ($dnszone in $DNSZones)
+    {
+    $dnsrecords = Get-DnsServerResourceRecord -ZoneName $dnszone
+    foreach ($i in $dnsrecords)
+        { 
+            foreach ($ip in $DeadIpAdresses)
+            {
+                if ($i.RecordData.IPv4Address -eq $ip)
+                    {
+                        Remove-DnsServerResourceRecord -ZoneName $dnszone -InputObject $i -Force
+                        
+                    }
+            }
+            foreach ($dns in $DeadDNSRecords)
+            {
+                if (($dns -eq $i.RecordData.NameServer) -or ($dns -eq $i.RecordData.DomainName))
+                    {
+                        Remove-DnsServerResourceRecord -ZoneName $dnszone -InputObject $i -Force
+                        
+                    }
+            }
+        
+        }
+    }
