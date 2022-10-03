fd0dfb0a08b9fd76b66e388774e01e571c75be93
Maksim Karimov <67639452+maxkarim@users.noreply.github.com>
2022-10-03 04:41:40 -0700

    Add files via upload

---
+++ disable_ad_user.ps1
@@ -0,0 +1,35 @@
+﻿Import-Module ActiveDirectory
+
+function Show-Menu
+{
+     param (
+           [string]$Title = 'My Menu'
+     )
+     cls
+     Write-Host "================ $Title ================"
+    
+     Write-Host "1: Нажми '1' заблокировать учетную запись."
+     Write-Host "2: Нажми '2' разблокироать учетную запись."
+     Write-Host "Q: Нажми 'Q' чтобы выйти."
+}
+do
+{
+     Show-Menu
+     $input = Read-Host "Выбери опцию:"
+     switch ($input)
+     {
+           '1' {
+                cls
+                'Заблокирование учетной записи #1'
+                Get-ADUser -Filter 'SamAccountName -eq "ArtSoft1"' -SearchBase "DC=chsz,DC=local" | Disable-ADAccount
+           } '2' {
+                cls
+                'Разблокирование учетной записи #2'
+                Get-ADUser -Filter 'SamAccountName -eq "ArtSoft1"' -SearchBase "DC=chsz,DC=local" | Enable-ADAccount
+           } 'q' {
+                return
+           }
+     }
+     pause
+}
+until ($input -eq 'q')
